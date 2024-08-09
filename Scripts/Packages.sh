5e8824f5a79975a65a45b1667a4734ff888aa9d7
YunHair <71691550+YunHair@users.noreply.github.com>
2024-08-09 15:57:48 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -40,8 +40,8 @@ UPDATE_PACKAGE "luci-app-mosdns" "sbwml/luci-app-mosdns" "v5"
 
 
 if [[ $WRT_REPO != *"lede"* ]]; then
-	UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
-        #UPDATE_PACKAGE "homeproxy" "muink/luci-app-homeproxy" "master"
+	#UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
+        UPDATE_PACKAGE "luci-app-homeproxy" "muink/luci-app-homeproxy" "master"
 	UPDATE_PACKAGE "mihomo" "morytyann/OpenWrt-mihomo" "main" "pkg"
 fi
 
