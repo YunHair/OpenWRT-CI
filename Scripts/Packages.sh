bc8cf26439f097a3eb6eb5a4163db56afa0d7886
YunHair <71691550+YunHair@users.noreply.github.com>
2024-08-14 00:19:21 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -29,11 +29,11 @@ UPDATE_PACKAGE "luci-app-unblockneteasemusic" "YunHair/luci-app-unblockneteasemu
 #UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 #UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main"
 #UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
+#UPDATE_PACKAGE "luci-app-unblockneteasemusic" "YunHair/luci-app-unblockneteasemusic" "js"
 
 UPDATE_PACKAGE "advancedplus" "YunHair/luci-app-advancedplus" "main"
 UPDATE_PACKAGE "gecoosac" "lwb1978/openwrt-gecoosac" "main"
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
-#UPDATE_PACKAGE "luci-app-unblockneteasemusic" "YunHair/luci-app-unblockneteasemusic" "js"
 UPDATE_PACKAGE "luci-app-multi-frpc" "YunHair/luci-app-multi-frpc" "main"
 UPDATE_PACKAGE "luci-app-wolplus" "YunHair/luci-app-wolplus" "main"
 UPDATE_PACKAGE "luci-app-mosdns" "sbwml/luci-app-mosdns" "v5"
