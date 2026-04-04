59ac50de44a5898e814d41b5a53e749c34bdea32
YunHair <71691550+YunHair@users.noreply.github.com>
2026-04-04 10:34:52 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -78,11 +78,8 @@ UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-ap
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
 UPDATE_PACKAGE "luci-app-advancedplus" "sirpdboy/luci-app-advancedplus" "main"
-
-#UPDATE_PACKAGE "appfilter" "destan19/OpenAppFilter" "master" "pkg"
-#UPDATE_PACKAGE "oaf" "destan19/OpenAppFilter" "master" "pkg"
 UPDATE_PACKAGE "OpenAppFilter" "destan19/OpenAppFilter" "master" 
-UPDATE_PACKAGE "luci-app-timecontrol" "sirpdboy/luci-app-timecontrol" "master" "pkg"
+UPDATE_PACKAGE "luci-app-timecontrol" "sirpdboy/luci-app-timecontrol" "master"
 UPDATE_PACKAGE "lucky" "gdy666/luci-app-lucky" "main" 
 UPDATE_PACKAGE "zapret" "remittor/zapret-openwrt" "zap1" "pkg"
 UPDATE_PACKAGE "luci-app-zapret" "remittor/zapret-openwrt" "zap1" "pkg"
