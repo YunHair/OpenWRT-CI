15bc6ea9a01e06b447c5e39cc2be6859b7e2e80e
YunHair <71691550+YunHair@users.noreply.github.com>
2026-08-22 23:04:33 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -80,6 +80,8 @@ UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
 UPDATE_PACKAGE "timecontrol" "sirpdboy/luci-app-timecontrol" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "axonhub gecoosac sing-box luci-app-homeproxy luci-app-timewol luci-app-wolplus luci-app-wolultra"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
+UPDATE_PACKAGE "yunhair-package" "YunHair/openwrt-package" "main" "" "usteer-ng luci-app-advancedplus luci-app-usteer"
+UPDATE_PACKAGE "kenzok8" "kenzok8/small-package" "main" "" "lucky luci-app-lucky oaf open-app-filter luci-app-oaf "
 
 #更新软件包版本
 UPDATE_VERSION() {
