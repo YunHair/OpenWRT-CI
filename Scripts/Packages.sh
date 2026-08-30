da0a636fddfb49f7e415c3ada63b667334fc78ba
YunHair <71691550+YunHair@users.noreply.github.com>
2026-08-30 14:45:19 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -81,7 +81,7 @@ UPDATE_PACKAGE "timecontrol" "sirpdboy/luci-app-timecontrol" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "axonhub gecoosac sing-box luci-app-homeproxy luci-app-timewol luci-app-wolplus luci-app-wolultra"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
-UPDATE_PACKAGE "yunhair-package" "YunHair/openwrt-package" "main" "" "luci-app-advancedplus lucky luci-app-lucky oaf open-app-filter luci-app-oaf wrtbwmon luci-app-wrtbwmon"
+UPDATE_PACKAGE "yunhair-package" "YunHair/openwrt-package" "main" "" "luci-app-advancedplus luci-app-lucky oaf open-app-filter luci-app-oaf wrtbwmon luci-app-wrtbwmon"
 #UPDATE_PACKAGE "kenzok8" "kenzok8/small-package" "main" "" "oaf open-app-filter luci-app-oaf "
 #UPDATE_PACKAGE "tailscale-community" "Tokisaki-Galaxy/luci-app-tailscale-community" "master" "" "luci-app-tailscale-community"
 
